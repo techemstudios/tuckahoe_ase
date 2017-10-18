8383865102f0b7387fe9f829bc0786625f2bf50b
joetechem <joe@techemstudios.com>
2017-10-18 10:58:56 -0400

    added/updated notes.py

---
+++ notes.py
@@ -0,0 +1,30 @@
+# 10/4
+    # Introductions
+    # what is game design
+        # what is programming/coding
+    # what is design thinking
+        # chair design example
+        # focus on gathering info on user wants/needs: empathy
+    # continued design thinking discussion with what makes a good game;
+    # what makes a not so good game
+    # design thinking interview sheets/play 3 games on hyperpad made
+
+    # Discuss good level design
+        # using points made during interviews and class disucssion on what makes a good game
+        # minimum of three levels
+    # Level-design sheets: sketch levels on paper
+
+# 10/11
+    # 10min to finish Level-design
+    # hyperpad
+        # use already created "blank" game "TES"
+        # assets needed are already downloaded for Students
+        # focus on pure level design, wait on character plaement/behaviors
+
+
+# 10/18
+    # hyperpad
+    # class walkthrough on character movement behaviors,
+    # and interaction of character to environment
+    # stress again --> everything is an object that is not programmed by itself
+    # you, must program i.e. create the rules/attributes for each object
